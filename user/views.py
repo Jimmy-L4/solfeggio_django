3f65c0e28e7bc9f0372a2ee644fb4bf4c3ecc3ee
Jimmy-L4 <2454357245@qq.com>
2022-10-24 20:18:09 +0800

    v1.2.8 添加帮助界面

--- user/views.py
+++ user/views.py
@@ -420,6 +420,17 @@ class UserNav(APIView):
                     },
                     'component': 'DictationResult',
                 },
+                {
+                    'name': 'help',
+                    'parentId': 0,
+                    'id': 1000,
+                    'meta': {
+                        'title': '帮助文档',
+                        'show': False,
+                    },
+                    'component': 'Help',
+
+                },
 
             ]
             if userSerializer.data['is_superuser']:
